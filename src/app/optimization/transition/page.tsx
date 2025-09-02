a67839498d925aa5517ee587e6dd9ddd935e5893
Nikita Zalevskii <nikita.zalevsky@amigochat.io>
2025-09-02 23:43:59 +0300

    add transition

--- src/app/optimization/transition/page.tsx
+++ src/app/optimization/transition/page.tsx
@@ -1,3 +1,22 @@
+'use client'
+
+import { SlowComponent } from '@/app/components/SlowComponent'
+import { useState, useTransition } from 'react'
+
 export default function Transition() {
-  return <div className="p-8">Transition</div>
+  const [isVisible, setIsVisible] = useState(false)
+  const [isPending, startTransition] = useTransition()
+
+  return (
+    <div className="p-8">
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md cursor-pointer font-bold mr-2"
+        onClick={() => startTransition(() => setIsVisible(!isVisible))}
+      >
+        {isVisible ? 'Hide' : 'Show'}
+      </button>
+      {isPending && <div>Loading...</div>}
+      {isVisible && <SlowComponent />}
+    </div>
+  )
 }
